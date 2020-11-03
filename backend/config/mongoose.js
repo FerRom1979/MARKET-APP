17f96f920377b03efb74209482f17a4a2605d78a
Leandro <leandroemanuelpaez2010@hotmail.com>
2020-11-03 16:59:27 -0300

    add .env

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
-const URI =
-  "mongodb+srv://market-app:1234@cluster0.knbkj.mongodb.net/market-app?retryWrites=true&w=majority";
-
+const URI = process.env.REACT_APP_MONGO_URI;
 mongoose
   .connect(URI, { useNewUrlParser: true })
   .then((db) => console.log("It was possible to connect to the database"))

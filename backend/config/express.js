8483fce2582338aa28ce9b4ed1d22ff059587352
Leandro <leandroemanuelpaez2010@hotmail.com>
2020-11-10 15:46:26 -0300

    modificaction in data(BasicTable)

--- backend/config/express.js
+++ backend/config/express.js
@@ -2,8 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const { mongoose } = require("./mongoose");
-const verifyToken = require("../controllers/verifyToken");
-const cors = require("cors");
+/* const verifyToken = require("../controllers/verifyToken");
+ */ const cors = require("cors");
 
 app.set("port", process.env.PORT || 5000);
 
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 });
 
 /* Routes */
-app.use("/products", verifyToken, require("../routes/product.route"));
+app.use("/products", /* verifyToken,  */ require("../routes/product.route"));
 app.use("/", require("../routes/auth.route"));
 
 app.listen(app.get("port"), () => {

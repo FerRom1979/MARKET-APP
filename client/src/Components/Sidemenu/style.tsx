0f6d27e0a3e1f248df7387f58f7157cb46264313
FerRom1979 <fernando_0135@hotmail.com>
2020-12-03 15:15:02 -0300

    incorporate custom theme

--- client/src/Components/Sidemenu/style.tsx
+++ client/src/Components/Sidemenu/style.tsx
@@ -1,73 +1,70 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles } from '@material-ui/core';
 
-const primary = '#607D8B';
-const defaultColor = '#BDBDBD';
 const drawerWidth = '200px';
-const usesStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
+const usesStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    drawer: {
+      [theme.breakpoints.up('sm')]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      background: theme.palette.primary.main,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up('sm')]: {
+        display: 'none',
+        background: theme.palette.secondary.main,
+      },
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
       width: drawerWidth,
-      flexShrink: 0,
+      background: theme.palette.secondary.main,
     },
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    background: '#512da8',
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
-      background: '#ff9800',
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      textDecoration: 'none',
     },
-  },
-  toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth,
-    background: '#ff9800',
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    textDecoration: 'none',
-  },
-  closeMenuButton: {
-    marginRight: 'auto',
-    marginLeft: 0,
-  },
-  listItem: {
-    textDecoration: 'none',
-    fontSize: '12px',
-    color: 'white',
-    background: '#ff9800',
-    '&:hover': {
-      backgroundColor: defaultColor,
+    closeMenuButton: {
+      marginRight: 'auto',
+      marginLeft: 0,
     },
-  },
-  icons: {
-    marginRight: '10px',
-  },
-  language: {
-    marginLeft: 'auto',
-  },
-  switch: {
-    marginRight: '20px',
-    marginLeft: '10px',
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: '20px',
-  },
-  button: {
-    backgroundColor: primary,
-    color: 'white',
-    marginLeft: 'auto',
-  },
-  list: {
-    textDecoration: 'none',
-  },
-}));
+    listItem: {
+      textDecoration: 'none',
+      fontSize: '12px',
+      color: 'white',
+      background: theme.palette.secondary.main,
+      '&:hover': {
+        backgroundColor: '#ff5722',
+      },
+    },
+    icons: {
+      marginRight: '10px',
+    },
+    switch: {
+      marginRight: '20px',
+      marginLeft: '10px',
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: '20px',
+    },
+    button: {
+      backgroundColor: theme.palette.action.selected,
+      color: 'white',
+      marginLeft: 'auto',
+    },
+    list: {
+      textDecoration: 'none',
+    },
+  }),
+);
 
 export default usesStyles;

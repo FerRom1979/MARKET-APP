69931d1f32f8892d6c4b43a2410558bc248a6314
Leandro <leandroemanuelpaez2010@hotmail.com>
2020-12-11 16:02:57 -0300

    refactoring product model

--- backend/models/productSchema.js
+++ backend/models/productSchema.js
@@ -3,9 +3,12 @@ const { Schema } = mongoose;
 
 const productSchema = new Schema({
   name: { type: String, required: true },
-  description: { type: String, required: true },
+  material: { type: String, required: true },
   quantity: { type: Number, required: true },
-  price: { type: Number, required: true },
+  measures: { type: String, required: true },
+  datePurchase: { type: Number, required: true },
+  provider: { type: String, required: true },
+  listPrice: { type: Number, required: true },
   finalPrice: { type: Number, required: true },
 });
 
